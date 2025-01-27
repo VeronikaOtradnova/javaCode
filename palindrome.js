ede20bd264bfc1d7eacad782b15e4f2404c5349d
Veronika Otradnova <nika.nsk.98@gmail.com>
2025-01-27 22:11:12 +0300

    Completed task from topic 4

---
+++ palindrome.js
@@ -0,0 +1,24 @@
+function getIsPrintableAscii(s) {
+  for (let i = 0; i < s.length; i++) {
+    const code = s.charCodeAt(i)
+    if (code < 32 || code > 126) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+function getIsPalindrome(s) {
+  if (s.length < 1 || s.length > 2 * 105 || !getIsPrintableAscii(s)) throw new Error('s is not valid');
+
+  const cleanS = s.toLowerCase().replace(/[^a-zа-яё0-9]/g, '');
+  const cleanSReversed = [...cleanS].reverse().join('');
+  console.log(cleanS, cleanSReversed);
+
+  if (cleanS === cleanSReversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
